a40588bc22c5052c49eb881bc722c8dca1901cda
Outmane Bzioui <bziouioutmane@gmail.com>
2025-10-28 01:17:02 +0100

    test black background

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
         <div className="max-w-[1440px] mx-auto min-h-screen">
           <Header />
