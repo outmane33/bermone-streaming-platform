ecff763acff738fef07372083dcc0d2619ef3e60
Outmane Bzioui <bziouioutmane@gmail.com>
2025-10-28 01:54:56 +0100

    test gradient background

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,19 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black overflow-x-hidden`}
       >
-        <div className="max-w-[1440px] mx-auto min-h-screen">
+        {/* Animated gradient background */}
+        <div className="fixed inset-0 -z-10">
+          {/* Main gradient orbs */}
+          <div className="absolute top-0 -left-4 w-96 h-96 bg-purple-500 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob"></div>
+          <div className="absolute top-0 -right-4 w-96 h-96 bg-blue-500 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
+          <div className="absolute -bottom-8 left-20 w-96 h-96 bg-pink-500 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
+          <div className="absolute bottom-20 right-20 w-96 h-96 bg-cyan-500 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob animation-delay-6000"></div>
+
+          {/* Additional subtle orbs for depth */}
+          <div className="absolute top-1/2 left-1/2 w-72 h-72 bg-indigo-500 rounded-full mix-blend-screen filter blur-3xl opacity-10 animate-blob animation-delay-3000"></div>
+        </div>
+
+        <div className="max-w-[1440px] mx-auto min-h-screen relative">
           <Header />
           {children}
         </div>
